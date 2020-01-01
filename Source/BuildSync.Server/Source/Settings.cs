05da0adc689c31d5fb95e263b4bcd2a0d45148fb
Tim Leonard <tim@programmingbytim.com>
2020-01-01 23:23:16 +0000

    Further fixing for setup package hell

--- Source/BuildSync.Server/Source/Settings.cs
+++ Source/BuildSync.Server/Source/Settings.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
-using System.Text.Json.Serialization;
 using BuildSync.Core.Utils;
 using BuildSync.Core.Downloads;
 using BuildSync.Core.Users;
