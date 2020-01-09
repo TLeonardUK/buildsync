ee86bef2ee2450f6396468de2fd1d8b6c4623c7f
Tim Leonard <tim@programmingbytim.com>
2020-01-09 00:56:27 +0000

    Fleshed out first pass of documentation

--- Source/BuildSync.Core/Source/Properties/AssemblyInfo.cs
+++ Source/BuildSync.Core/Source/Properties/AssemblyInfo.cs
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 [assembly: Guid("1d25dae9-8b10-4721-8911-789813751186")]
 
 // AUTO GENERATED
-[assembly: AssemblyVersion("1.0.0.52")]
-[assembly: AssemblyFileVersion("1.0.0.52")]
+[assembly: AssemblyVersion("1.0.0.60")]
+[assembly: AssemblyFileVersion("1.0.0.60")]
 // END AUTO GENERATED
